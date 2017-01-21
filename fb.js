3695908f41e3fedc823c40491a20273b6dfe2c78
Bryan <bryan.startertech@gmail.com>
2017-01-21 12:02:43 +0800

    add fb routes

---
+++ fb.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const logger = require("./logger");
+const fetch = require("isomorphic-fetch");
+
+const TOKEN = "this_is_my_token";
+
+router.get("/webhook", (req, res) => {
+	if (req.query['hub.mode'] === 'subscribe' &&
+		req.query['hub.verify_token'] === TOKEN) {
+		logger.info("Validating webhook");
+		res.status(200).send(req.query['hub.challenge']);
+	} else {
+		logger.error("Failed validation. Make sure the validation tokens match.");
+		res.sendStatus(403);
+	}
+});
